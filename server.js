8ab705c2cf95f138396e9ffdeebe3f6c849bac6a
Ahmed Yasser <ahmedreo4@gmail.com>
2023-06-23 17:54:33 +0300

    Create Order

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
-const axios = require('axios');
+const SallaAPIFactory = require("@salla.sa/passport-strategy");
 const PORT = process.env.PORT || 3030;
+
+const SallaAPI = new SallaAPIFactory({
+  clientID: "6b2143a5-148a-4c4e-b6ee-2f2d975640b8",
+  clientSecret: "aa8d7f95bdca4ed81521cfade2f92275",
+  callbackURL: "http://localhost:8081/oauth/callback",
+});
+
 
 
 // your code
@@ -9,82 +16,18 @@ const PORT = process.env.PORT || 3030;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// const options = {
-//   method: 'POST',
-//   url: 'https://accounts.salla.sa/oauth2/token',
-//   headers: { 'Content-Type': 'application/json' },
-//   data: {
-// client_id: '6b2143a5-148a-4c4e-b6ee-2f2d975640b8',
-// client_secret: 'aa8d7f95bdca4ed81521cfade2f92275',
-// grant_type: 'authorization_code',
-// code: 'CODE_PARAMETER',
-// scope: 'offline_access',
-// redirect_uri: 'REDIRECT_URI'
-//   }
-// };
-
-// try {
-//   const { data } = await axios.request(options);
-//   console.log(data);
-// } catch (error) {
-//   console.error(error);
-// }
-
 app.post('/test', async (req, res) => {
-  // const requestHMAC = req.headers["x-salla-signature"];
   const event = req.body.event
 
   if (event === 'product.created') {
-    // axios.post('https://accounts.salla.sa/oauth2/token',
-    //   {
-    //     client_id: '6b2143a5-148a-4c4e-b6ee-2f2d975640b8',
-    //     client_secret: 'aa8d7f95bdca4ed81521cfade2f92275',
-    //     grant_type: 'authorization_code',
-    //     code: 'CODE_PARAMETER',
-    //     scope: 'offline_access',
-    //     redirect_uri: 'REDIRECT_URI'
-    //   },
-    //   {
-    //     headers: { 'Content-Type': 'application/json' }
-    //   }).then((response) => {
-    //     console.log(response.data)
-    //   }).catch((err) => {
-    //     console.log(err)
-    //   })
-    // const options = {
-    //   method: 'POST',
-    //   url: 'https://api.salla.dev/admin/v2/orders',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     Accept: 'application/json',
-    //     Authorization: 'Bearer 195c3ebff1439d91db5f473a92863dc4'
-    //   },
-    //   data: {
-    //     customer_id: 1,
-    //     shipping_address: {
-    //       country_id: 566146469,
-    //       city_id: 2097610897,
-    //       block: 'Om El 9823489237',
-    //       street_number: 'jmoh El 8912749823764',
-    //       address: 'building 124234324, floor 212423',
-    //       postal_code: '23874982374',
-    //       geocode: '21.4283792, 21.4283792'
-    //     },
-    //     payment: { status: 'paid', method: 'credit_card' },
-    //     products: [{ id: 2 }]
-    //   }
-    // };
-
-    // try {
-    //   const { data } = await axios.request(options);
-    //   console.log(data);
-    // } catch (error) {
-    //   console.error(error);
-    // }
+    SallaAPI.requestNewAccessToken(SallaAPI.getRefreshToken())
+      .then(({ accessToken, newRefreshToken }) => {
+        console.log(accessToken, newRefreshToken)
+      })
+      .catch((err) => res.send(err));
   }
 
   console.log(req.headers)
-  res.send('recieved post');
 
   // 
 })
